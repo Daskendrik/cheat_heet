e0232413e5faf38f7d81a6ee48b7edae2609ee05
Dashka_W <Daskendrik@users.noreply.github.com>
2023-09-01 15:10:11 +0300

    красота

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-function showBlock(id){
-    if($(`#${id}`).hasClass("displayBlock")) {
-        $(`#${id}`).removeClass("displayBlock");
-    } else {
-        $(`#${id}`).addClass("displayBlock");
-    }
+function showBlock(id) {
+  if ($(`#${id}`).hasClass('displayBlock')) {
+    $(`#${id}`).removeClass('displayBlock');
+  } else {
+    $(`#${id}`).addClass('displayBlock');
+  }
 }
 
-
 $('.btn').on('click', function (e) {
-    showBlock(e.currentTarget.attributes[1].value)
+  showBlock(e.currentTarget.attributes[1].value);
 });
